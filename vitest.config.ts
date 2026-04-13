289a3b7dd51b767fa1c8c70c7357230c8c3d2c36
lucas081297 <65791119+lucas081297@users.noreply.github.com>
2026-04-13 19:37:33 -0300

    test: configure Vitest and add Jest-DOM setup
    - Add `vitest.config.ts` for test configuration, including `jsdom` environment and alias setup.
    - Install `@testing-library/jest-dom` and update `package.json` dependencies.
    - Include global `Jest-DOM` setup file for enhanced testing capabilities.

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/__tests__/setup.ts'],
+    alias: {
+      '#': './src',
+    },
+  },
+})
